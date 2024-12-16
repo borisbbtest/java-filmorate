4f55a1e3db0d1e33943833cb983382588d003efc
Boris <boris.z.077@gmail.com>
2024-12-16 07:46:58 +0300

    add project

--- src/main/java/ru/yandex/practicum/filmorate/controller/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GlobalExceptionHandler.java
@@ -13,7 +13,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<String> handleValidationException(ValidationException ex) {
         // Возвращает сообщение об ошибке с кодом 500
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
 }
