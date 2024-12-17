0be2326068c7c8c56f7c8867f79965e99c1ceea0
Boris <boris.z.077@gmail.com>
2024-12-17 07:46:23 +0300

    add project

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -5,9 +5,14 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserStorage {
+
     User addUser(User user);
+
     User updateUser(User user);
+
     User getUserById(int id);
+
     void deleteUser(int id);
+
     List<User> getAllUsers();
 }
